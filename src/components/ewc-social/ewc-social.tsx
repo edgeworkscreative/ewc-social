bc5d53c551f3727b7b1531bde758688abbe62cdc
Darren <darren@edgeworkscreative.com>
2018-09-13 15:52:57 -0400

    Initial commit

---
+++ src/components/ewc-social/ewc-social.tsx
@@ -0,0 +1,48 @@
+import {Component, Prop} from '@stencil/core';
+import 'ionicons';
+
+@Component({
+  tag: 'ewc-social',
+  styleUrl: 'ewc-social.css',
+  shadow: true
+})
+export class EwcSocial {
+
+  @Prop() facebook: string;
+  @Prop() instagram: string;
+  @Prop() pinterest: string;
+  @Prop() linkedin: string;
+  @Prop() twitter: string;
+  @Prop() googlePlus: string;
+  @Prop() youtube: string;
+  @Prop() vimeo: string;
+  @Prop() snapchat: string;
+  @Prop({mutable:true}) labels: boolean = true;
+
+  render() {
+    return (
+      <div class="social-container">
+        <ul class="social-ul">
+
+          {(this.facebook) ? <li><a href={this.facebook}><ion-icon name="logo-facebook"></ion-icon>{(this.labels) ? 'Facebook' : ''}</a></li> : ''}
+
+          {(this.instagram) ? <li><a href={this.instagram}><ion-icon name="logo-instagram"></ion-icon>{(this.labels) ? 'Instagram' : ''}</a></li> : ''}
+
+          {(this.pinterest) ? <li><a href={this.pinterest}><ion-icon name="logo-pinterest"></ion-icon>{(this.labels) ? 'Pinterest' : ''}</a></li> : ''}
+
+ {(this.linkedin) ? <li><a href={this.linkedin}><ion-icon name="logo-linkedin"></ion-icon>{(this.labels) ? 'Linkedin' : ''}</a></li> : ''}
+
+ {(this.twitter) ? <li><a href={this.twitter}><ion-icon name="logo-twitter"></ion-icon>{(this.labels) ? 'Twitter' : ''}</a></li> : ''}
+
+ {(this.youtube) ? <li><a href={this.youtube}><ion-icon name="logo-youtube"></ion-icon>{(this.labels) ? 'Youtube' : ''}</a></li> : ''}
+
+ {(this.vimeo) ? <li><a href={this.vimeo}><ion-icon name="logo-vimeo"></ion-icon>{(this.labels) ? 'Vimeo' : ''}</a></li> : ''}
+
+ {(this.snapchat) ? <li><a href={this.snapchat}><ion-icon name="logo-snapchat"></ion-icon>{(this.labels) ? 'Snapchat' : ''}</a></li> : ''}
+
+
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ src/components/ewc-social/readme.md
@@ -0,0 +1,26 @@
+# ewc-social
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property     | Attribute     | Description | Type      |
+| ------------ | ------------- | ----------- | --------- |
+| `facebook`   | `facebook`    |             | `string`  |
+| `googlePlus` | `google-plus` |             | `string`  |
+| `instagram`  | `instagram`   |             | `string`  |
+| `labels`     | `labels`      |             | `boolean` |
+| `linkedin`   | `linkedin`    |             | `string`  |
+| `pinterest`  | `pinterest`   |             | `string`  |
+| `snapchat`   | `snapchat`    |             | `string`  |
+| `twitter`    | `twitter`     |             | `string`  |
+| `vimeo`      | `vimeo`       |             | `string`  |
+| `youtube`    | `youtube`     |             | `string`  |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
